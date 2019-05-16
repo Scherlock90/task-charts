365296dc5248b1bd14e2dda912678e0f28b704c1
Sebastian Napora <sebastimus@o2.pl>
2019-05-16 14:27:24 +0200

    Updates

---
+++ precache-manifest.e594b05f8762542759b72d05019530c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c475490c435375238db0ee3ffe6c3ce1",
+    "url": "/task-charts/index.html"
+  },
+  {
+    "revision": "138507ecb14a6a5142f1",
+    "url": "/task-charts/static/css/main.bfd49545.chunk.css"
+  },
+  {
+    "revision": "2c6d03ef7704e57b80a7",
+    "url": "/task-charts/static/js/2.5bdb0e6e.chunk.js"
+  },
+  {
+    "revision": "138507ecb14a6a5142f1",
+    "url": "/task-charts/static/js/main.14f2bd27.chunk.js"
+  },
+  {
+    "revision": "b48b834c629d8cd3dc92",
+    "url": "/task-charts/static/js/runtime~main.fc413007.js"
+  }
+]);
