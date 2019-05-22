cf20e60e4a71770abf9e4b3a9cafcd4b7beffa3b
Sebastian Napora <sebastimus@o2.pl>
2019-05-22 18:18:37 +0200

    fromat document

--- src/App.js
+++ src/App.js
@@ -12,39 +12,39 @@ let continent = "Continent";
 
 Array.prototype.sum = function (prop) {
   var total = 0
-  for ( var i = 0, _len = this.length; i < _len; i++ ) {
-      total += this[i][prop]
+  for (var i = 0, _len = this.length; i < _len; i++) {
+    total += this[i][prop]
   }
   return total
 }
 
-function groupBy(list, keyGetter) {
-  const map = new Map();
-  list.forEach((item) => {
-       const key = keyGetter(item);
-       const collection = map.get(key);
-       if (!collection) {
-           map.set(key, [item]);
-       } else {
-           collection.push(item);
-       }
-  });
-  return map;
-}
+// function groupBy(list, keyGetter) {
+//   const map = new Map();
+//   list.forEach((item) => {
+//     const key = keyGetter(item);
+//     const collection = map.get(key);
+//     if (!collection) {
+//       map.set(key, [item]);
+//     } else {
+//       collection.push(item);
+//     }
+//   });
+//   return map;
+// }
 
-function sumProperty(arr, type) {
-  return arr.reduce((total, obj) => {
-    if (typeof obj[type] === 'string') {
-      return total + Number(obj[type]);
-    }
-    return total + obj[type];
-  }, 0);
-}
+// function sumProperty(arr, type) {
+//   return arr.reduce((total, obj) => {
+//     if (typeof obj[type] === 'string') {
+//       return total + Number(obj[type]);
+//     }
+//     return total + obj[type];
+//   }, 0);
+// }
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {      
+    this.state = {
       population: [],
       minor: [],
       counts: [],
@@ -54,7 +54,7 @@ export default class App extends React.Component {
     this.compareBy.bind(this);
     this.sortBy.bind(this);
   }
-    
+
   componentDidMount() {
     axios.get('https://pkgstore.datahub.io/core/population/population_json/data/43d34c2353cbd16a0aa8cadfb193af05/population_json.json')
       .then(res => {
@@ -62,13 +62,13 @@ export default class App extends React.Component {
           population: res.data,
         })
       })
-      axios.get('https://pkgstore.datahub.io/core/country-codes/country-codes_json/data/471a2e653140ecdd7243cdcacfd66608/country-codes_json.json')
+    axios.get('https://pkgstore.datahub.io/core/country-codes/country-codes_json/data/471a2e653140ecdd7243cdcacfd66608/country-codes_json.json')
       .then(res => {
-        let countryWithEuro = res.data.map(function(car) {
+        let countryWithEuro = res.data.map(function (car) {
           return car[currencyName] === "Euro"
-        }).reduce(function(previousValue, currentValue) {
+        }).reduce(function (previousValue, currentValue) {
           return previousValue + currentValue
-        });     
+        });
         let countryInTheWorld = res.data;
         this.setState({
           minor: res.data,
@@ -79,71 +79,73 @@ export default class App extends React.Component {
         console.log(countryWithEuro);
         console.log(countryInTheWorld);
       })
-    }
+  }
 
-    compareBy(key) {
-      return function (a, b) {
-        if (a[key] < b[key]) return -1;
-        if (a[key] > b[key]) return 1;
-        return 0;
-      };
-    }
-   
-    sortBy(key) {
-      let arrayCopy = [...this.state.minor];
-      arrayCopy.sort(this.compareBy(key));
-      this.setState({minor: arrayCopy});
-    }
+  compareBy(key) {
+    return function (a, b) {
+      if (a[key] < b[key]) return -1;
+      if (a[key] > b[key]) return 1;
+      return 0;
+    };
+  }
+
+  sortBy(key) {
+    let arrayCopy = [...this.state.minor];
+    arrayCopy.sort(this.compareBy(key));
+    this.setState({ minor: arrayCopy });
+  }
 
   render() {
-    const { population, minor, counts, countryWithEuro, countryInWorld } = this.state;  
+    const { population, minor, counts, countryWithEuro, countryInWorld } = this.state;
 
-    const dataList = population.length ? (population.slice(11449, 11506).filter((populationData, i) => {
+    const dataList = population.length ? (population.slice(11449, 11506)
+      .filter((populationData, i) => {
       return (
         <ul key={i}>
           {populationData.Year}
           {populationData.Value}
         </ul>
       );
-    }).map(ee => ee)
-    ) : (
-        <div className="center">No data yet! </div>
-    )
-    const dataList2 = minor
-    .filter((album, i) => {
-      return (
-        <ul key={album.id}>
-          {album[currencyMinor]}
-          {album[currencyName]}
-          {album[continent]}
-        </ul>
-      );
     })
       .map(ee => ee)
-      // .filter(function(hero) {
-      //   if(hero[currencyName] === "Euro"){
-      //     return hero[currencyName]
-      //   }
-      // });
-      
-      const dataList3 = minor
-      .filter(function(hero) {
-        if(hero[currencyName] === "Euro"){
+    ) : (
+        <div className="center">No data yet! </div>
+      )
+    const dataList2 = minor
+      .filter((album, i) => {
+        return (
+          <ul key={album.id}>
+            {album[currencyMinor]}
+            {album[currencyName]}
+            {album[continent]}
+          </ul>
+        );
+      })
+      .map(ee => ee)
+      .filter(function (hero) {
+        if (hero[currencyName] === "Euro") {
           return hero[currencyName]
         }
       });
-      const dataList4 = minor
+
+    // const dataList3 = minor
+    // .filter(function(hero) {
+    //   if(hero[currencyName] === "Euro"){
+    //     return hero[currencyName]
+    //   }
+    // });
+    // const dataList4 = minor
     //   .filter(function(hero) {
     //     if (hero[continent] === "EU"){
     //     return hero[continent]
     //   }
     // });
 
-    const grouped = groupBy(dataList4, pet => pet[currencyName]);
-    const grouped2 = groupBy(dataList4, pet => pet[continent]);
-    let totalAmount = ( sumProperty(dataList3, currencyMinor) ); 
-    // let totalCountry = (sumProperty(dataList2, currencyName ))
-    let totEuro = dataList3.sum(currencyName).length;
+    // const grouped = groupBy(dataList4, pet => pet[currencyName]);
+    // const grouped2 = groupBy(dataList4, pet => pet[continent]);
+    // let totalAmount = ( sumProperty(dataList3, currencyMinor) ); 
+    // // let totalCountry = (sumProperty(dataList2, currencyName ))
+    // let totEuro = dataList3.sum(currencyName).length;
     // const group = dataList2.groupBy(currencyName);
     // console.log(minor);
     // console.log(minor.length);
@@ -158,14 +160,15 @@ export default class App extends React.Component {
       <div className="containerLoader" style={containerLoader}>
         <div className="card z-depth-0 project-summary thumb">
           <div className="card-content grey-text text-darken-3 containerPost">
-            <LineCharts data={dataList} />
-            <BarCharts  data={dataList2} />
+            <div className="chartsContainer">
+              <LineCharts data={dataList} />
+            </div>
+            <div className="chartsContainer">
+              <BarCharts data={dataList2} />
+            </div>
             <div>Countries with the euro currency: {countryWithEuro} </div>
             <div>The number of countries in the world: {countryInWorld} </div>
-            {}
-            {/* <div onClick={() => this.sortBy(currencyName)}>Sort</div> */}
-            {/* <button onClick={e => this.sumEuro(e)}> Sum Euro</button> */}
-          </div>          
+          </div>
         </div>
       </div>
     )
@@ -177,8 +180,8 @@ function LineCharts(props) {
     <LineChart width={900} height={250} data={props.data}
       margin={{ top: 5, right: 30, left: 50, bottom: 5 }}>
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis tick={<CustomizedAxisTick/>}   type="category" interval="preserveStartEnd"   label={{ value: 'Years', position: 'insideBottomRight', offset: -10 }} dataKey="Year" />
-      <YAxis interval="preserveStartEnd" type="number"  domain={['auto', 'auto']}  label={{ value: 'Population', angle: -90, position: 'insideLeft', offset: -20 }}  />
+      <XAxis tick={<CustomizedAxisTick />} type="category" interval="preserveStartEnd" label={{ value: 'Years', position: 'insideBottomRight', offset: -10 }} dataKey="Year" />
+      <YAxis interval="preserveStartEnd" type="number" domain={['auto', 'auto']} label={{ value: 'Population', angle: -90, position: 'insideLeft', offset: -20 }} />
       <Tooltip />
       <Legend />
       <Line type="monotone" dataKey="Value" stroke="#8884d8" />
@@ -200,18 +203,17 @@ function BarCharts(props) {
   )
 }
 
-class CustomizedAxisTick extends React.Component{
-  render () {
-  const {x, y, stroke, payload} = this.props;
-      
-  return (
-      <g transform={`translate(${x},${y})`}>
-      <text x={0} y={0} dy={4} textAnchor="end" fill="#666" fontSize="12px" transform="rotate(-25)">{payload.value}</text>
-    </g>
-  );
-}
-}
+class CustomizedAxisTick extends React.Component {
+  render() {
+    const { x, y, stroke, payload } = this.props;
 
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={4} textAnchor="end" fill="#666" fontSize="12px" transform="rotate(-25)">{payload.value}</text>
+      </g>
+    );
+  }
+}
 
 const containerLoader = {
   display: 'flex',
